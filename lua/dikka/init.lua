704bacd6e706ce26fac530fbf11c504a67eeab22
Sett17 <raik.rohde@gmail.com>
2023-07-09 00:17:35 +0200

    feat: add new key mappings for Dikka
    Add new key mappings for Dikka plugin, including remapping for visual mode, navigation, delete, copy, and search. Also include key mappings for undotree, lazygit, and cellular automaton plugins.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -1,8 +1,31 @@
 require('dikka.scrolloff')
 -- REMAPS
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<C-s>', vim.cmd.w)
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>+2<CR>gv=gv")
+
+vim.keymap.set('n', '<C-e>', '5<C-e>') 
+vim.keymap.set('n', '<C-y>', '5<C-y>') 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
+vim.keymap.set('i', '<C-c>', '<Esc>')
+vim.keymap.set('n', 'Q', '<nop>')
+
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- CONFIGS
 vim.opt.clipboard = 'unnamedplus'
@@ -25,7 +48,7 @@ vim.opt.inccommand = "nosplit"
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 16
 vim.opt.signcolumn = "yes:1"
 
 vim.opt.updatetime = 500
@@ -69,33 +92,49 @@ require('lazy').setup({
 			},
 			{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-	},
-    {
-        'folke/which-key.nvim',
-        lazy = true,
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
     },
-	{
-		'mbbill/undotree',
-	},
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
+        'mbbill/undotree',
+    },
     {
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
+    },
+    {
+        'github/copilot.vim',
+        lazy = true,
+    },
+    {
+        'eandrju/cellular-automaton.nvim',
     },
 })
 
@@ -134,7 +173,12 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- lsp-zero
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset({
+  manage_nvim_cmp = {
+    set_sources = 'recommended',
+    set_extra_mappings = false,
+  }
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
@@ -142,8 +186,18 @@ end)
 
 lsp.setup()
 
+local cmp = require('cmp')
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+  }
+})
+
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- lazygit
 vim.keymap.set("n", "<leader>lg", vim.cmd.LazyGit)
+
+-- cellular automaton
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
