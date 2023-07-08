3f927323ff060fbbe450c806659bc7c2adb5722b
Sett17 <raik.rohde@gmail.com>
2023-07-09 01:12:15 +0200

    chore: update dikka/init.lua
    Update the dikka/init.lua file with new features and fixes.
    
    - Update the vim keymap for the 'K' command in visual mode.
    - Update the vim keymap for the '<C-e>' and '<C-y>' commands in normal mode.
    - Update the vim keymap for the ';' command in normal mode.
    - Install the lazy.nvim plugin if it is not already installed.
    - Add new plugins: rose-pine/neovim, VonHeikemen/lsp-zero.nvim, nvim-telescope/telescope.nvim, github/copilot.vim, eandrju/cellular-automaton.nvim, ThePrimeagen/harpoon, echasnovski/mini.ai, echasnovski/mini.comment.
    - Configure the nvim-treesitter plugin to highlight syntax.
    - Configure the lsp-zero plugin to manage nvim-cmp and set default keymaps.
    - Configure the cmp plugin to use the Enter key to confirm selections.
    - Configure the undotree plugin to allow saving and restoring undo history.
    - Add new keymap for the LazyGit command in normal mode.
    - Add new keymap for the CellularAutomaton command in normal mode.
    - Add new keymap for the mark.add_file command in normal mode.
    - Add new keymap for the ui.toggle_quick_menu command in normal mode.
    - Add new keymap for the ui.nav_file command in normal mode.
    - Configure the mini.ai plugin.
    - Configure the mini.comment plugin.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -5,10 +5,10 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<C-s>', vim.cmd.w)
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '>+2<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
-vim.keymap.set('n', '<C-e>', '5<C-e>') 
-vim.keymap.set('n', '<C-y>', '5<C-y>') 
+vim.keymap.set('n', '<C-e>', '5<C-e>')
+vim.keymap.set('n', '<C-y>', '5<C-y>')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
@@ -25,7 +25,9 @@ vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('n', ';', ':')
 
 -- CONFIGS
 vim.opt.clipboard = 'unnamedplus'
@@ -59,47 +61,48 @@ vim.opt.colorcolumn = "80"
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
+    print('Done.')
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	{
-		'rose-pine/neovim',
-		name = 'rose-pine'
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			build = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-			},
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine'
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     },
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
         -- or                              , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -131,10 +134,20 @@ require('lazy').setup({
     },
     {
         'github/copilot.vim',
-        lazy = true,
     },
     {
         'eandrju/cellular-automaton.nvim',
+    },
+    {
+        'ThePrimeagen/harpoon',
+    },
+    {
+        'echasnovski/mini.ai',
+        version = '*'
+    },
+    {
+        'echasnovski/mini.comment',
+        version = '*'
     },
 })
 
@@ -150,47 +163,52 @@ vim.keymap.set('n', '<C-p>', tele.git_files, {})
 vim.keymap.set('n', '<leader>ps', tele.live_grep, {})
 
 -- treesitter
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"javascript", "go", "rust", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "javascript", "go", "rust", "typescript", "c", "lua", "vim", "vimdoc", "query" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
 
 -- lsp-zero
 local lsp = require('lsp-zero').preset({
-  manage_nvim_cmp = {
-    set_sources = 'recommended',
-    set_extra_mappings = false,
-  }
+    manage_nvim_cmp = {
+        set_sources = 'recommended',
+        set_extra_mappings = false,
+    }
 })
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set({ 'n', 'x' }, '<leader>ff', function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+    end, opts)
 end)
 
 lsp.setup()
 
 local cmp = require('cmp')
 cmp.setup({
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-  }
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }
 })
 
 -- undotree
@@ -201,3 +219,24 @@ vim.keymap.set("n", "<leader>lg", vim.cmd.LazyGit)
 
 -- cellular automaton
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+
+-- harpoon
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+
+-- mini.ai
+require('mini.ai').setup()
+
+-- mini.comment
+require('mini.comment').setup()
+
+
