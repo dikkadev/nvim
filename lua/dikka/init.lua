28b10db36f7b68b8284830e564d14160a27e62e0
Sett17 <raik.rohde@gmail.com>
2023-07-10 20:07:05 +0200

    feat: add auto-session plugin
    This commit adds the auto-session plugin to the project. The plugin allows for managing and restoring Vim sessions automatically.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -177,6 +177,9 @@ require('lazy').setup({
     {
         'eandrju/cellular-automaton.nvim',
     },
+    {
+        'rmagatti/auto-session',
+    },
 })
 --PLUGINSEND
 
@@ -325,3 +328,16 @@ end, { silent = true })
 
 -- mini-surround
 require('mini.surround').setup()
+
+-- auto-session
+require("auto-session").setup {
+  log_level = "error",
+
+  cwd_change_handling = {
+    restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+    pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+    post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+      require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+    end,
+  },
+}
