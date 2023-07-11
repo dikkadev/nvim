3024ccb0ca1fd631318bcd588d68ae97cb314a5e
Sett17 <raik.rohde@gmail.com>
2023-07-11 19:08:16 +0200

    feat: add keymap for toggling wrap
    Add a keymap for toggling wrap in the dikka init.lua file.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -30,7 +30,7 @@ vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('x', '<leader>p', '|_dP')
 
--- vim.keymap.set('n', ';', ':')
+vim.keymap.set('n', '<leader>w', ':set wrap!<CR>')
 
 -- CONFIGS
 vim.opt.clipboard = 'unnamedplus'
