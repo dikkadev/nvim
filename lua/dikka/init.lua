f8c4780dbbb9658848e1dbc7106ff5d5702128cb
Sett17 <raik.rohde@gmail.com>
2023-07-15 17:12:08 +0200

    feat: add telescope-file-browser extension
    Add the telescope-file-browser extension to the configuration file. This extension replaces netrw and provides a file browser inside Telescope.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -166,6 +166,9 @@ require('lazy').setup({
         'nvim-telescope/telescope-ui-select.nvim',
     },
     {
+        "nvim-telescope/telescope-file-browser.nvim",
+    },
+    {
         'ThePrimeagen/harpoon',
     },
 
@@ -461,3 +464,28 @@ require("FTerm").setup({
 })
 vim.keymap.set('n', '<A-z>', '<CMD>lua require("FTerm").toggle()<CR>')
 vim.keymap.set('t', '<A-z>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
+-- telescope-file-browser
+require("telescope").setup {
+    extensions = {
+        file_browser = {
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            -- mappings = {
+            --   ["i"] = {
+            --     -- your custom insert mode mappings
+            --   },
+            --   ["n"] = {
+            --     -- your custom normal mode mappings
+            --   },
+            -- },
+        },
+    },
+}
+require("telescope").load_extension "file_browser"
+vim.api.nvim_set_keymap(
+    "n",
+    "<space>pv",
+    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { noremap = true }
+)
