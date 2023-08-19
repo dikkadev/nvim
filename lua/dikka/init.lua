39ee99d47e0a0c2e0577d22374c5151d6dcd7acf
Sett17 <raik.rohde@gmail.com>
2023-08-19 11:12:13 +0200

    chore: update lualine configuration
    Use relative path for displaying filename in lualine configuration.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -103,6 +103,14 @@ require('lazy').setup({
         'nvim-lualine/lualine.nvim',
         opts = {
             theme = 'iceberg_dark',
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1,
+                    },
+                },
+            },
         },
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     },
