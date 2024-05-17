c3669d8029965c0a1cac590103fcc5671a32c826
Raik Rohde <raik.rohde@eviden.com>
2024-05-17 09:47:58 +0200

    feat(lua): integrate gitsigns.nvim with custom keybindings
    Introduce gitsigns.nvim plugin to the project, providing an enhanced Git integration experience within Neovim. This configuration includes custom keybindings for hunk navigation, resetting hunks and buffers, blaming lines, diffing, and toggling visibility of deleted lines. This addition aims to streamline the developer's workflow by incorporating Git operations directly into the editor environment, making it easier to manage changes and understand code modifications contextually.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -795,6 +795,48 @@ require('lazy').setup({
     {
         'tjdevries/templ.nvim',
     },
+    {
+        'lewis6991/gitsigns.nvim',
+        init = function(_)
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ ']c', bang = true })
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end)
+
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ '[c', bang = true })
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end)
+
+                    map('n', '<leader>hr', gitsigns.reset_hunk)
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('n', '<leader>hR', gitsigns.reset_buffer)
+                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+                    map('n', '<leader>hd', gitsigns.diffthis)
+                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+                    map('n', '<leader>td', gitsigns.toggle_deleted)
+
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            }
+        end,
     },
 })
 
