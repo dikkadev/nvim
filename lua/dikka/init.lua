ad73eaa1d764d15856ed7bc5f112f80a72cb111f
Raik Rohde <raik.rohde@eviden.com>
2024-07-08 10:38:20 +0200

    fix(ui): change Visual highlight color
    Updated the Visual highlight color to improve readability and visual consistency.

--- lua/dikka/init.lua
+++ lua/dikka/init.lua
@@ -136,7 +136,7 @@ require('lazy').setup({
         'nyoom-engineering/oxocarbon.nvim',
         init = function()
             vim.cmd [[colorscheme oxocarbon]]
-            vim.cmd [[ hi Visual guifg=#55559b ]]
+            vim.cmd [[ hi Visual guifg=#17172b ]]
             vim.cmd [[ hi Visual guibg=#e1e132 ]]
         end
     },
