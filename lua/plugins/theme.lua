65789e984600ac37cdeb065c6bc7b076dc83a568
Raik Rohde <raik.rohde@eviden.com>
2024-10-10 14:15:15 +0200

    feat(theme): add Onedark theme support
    Integrated the Onedark.nvim theme into the existing theme setup, providing a range of customizations and visual improvements through its configuration options, enhancing the overall user interface experience.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,10 +3,16 @@ return {
         'nyoom-engineering/oxocarbon.nvim',
         dependencies = {
             'ofirgall/ofirkai.nvim',
+            'navarasu/onedark.nvim',
         },
         init = function()
             -- vim.cmd [[colorscheme oxocarbon]]
-            vim.cmd [[colorscheme ofirkai-darkblue]]
+            -- vim.cmd [[colorscheme ofirkai-darkblue]]
+            require('onedark').setup {
+                style = 'deep'
+            }
+            require('onedark').load()
+
             vim.cmd [[ hi Visual guifg=#17172b ]]
             vim.cmd [[ hi Visual guibg=#e1e132 ]]
             -- vim.cmd [[ hi Normal guibg=#11131D ]]
