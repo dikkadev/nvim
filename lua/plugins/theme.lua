6bb5a1fcafc78350fc2368b4d5f609c02dc0049f
Sett17 <raik.rohde@gmail.com>
2024-10-10 11:13:45 +0200

    feat(theme): update color scheme to use ofirkai-darkblue
    This commit changes the theme to use ofirkai-darkblue instead of oxocarbon, giving the user a fresh new look.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,10 +2,11 @@ return {
     {
         'nyoom-engineering/oxocarbon.nvim',
         dependencies = {
-
+            'ofirgall/ofirkai.nvim',
         },
         init = function()
-            vim.cmd [[colorscheme oxocarbon]]
+            -- vim.cmd [[colorscheme oxocarbon]]
+            vim.cmd [[colorscheme ofirkai-darkblue]]
             vim.cmd [[ hi Visual guifg=#17172b ]]
             vim.cmd [[ hi Visual guibg=#e1e132 ]]
             -- vim.cmd [[ hi Normal guibg=#11131D ]]
