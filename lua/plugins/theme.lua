cbe51e5face9678249cb9c7c7fab66854d6c1631
Sett17 <raik.rohde@gmail.com>
2024-10-11 09:31:30 +0200

    chore(theme): update theme dependencies and color scheme initialization
    Updated the theme dependencies to include new themes and adjusted the initialization to reflect the changes, making it a bit more versatile with options.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,16 +3,16 @@ return {
         'nyoom-engineering/oxocarbon.nvim',
         dependencies = {
             'ofirgall/ofirkai.nvim',
-            'navarasu/onedark.nvim',
+            'folke/tokyonight.nvim',
+            { 'rose-pine/neovim', name = 'rose-pine' },
+            'tiagovla/tokyodark.nvim',
+            'projekt0n/github-nvim-theme'
         },
         init = function()
             -- vim.cmd [[colorscheme oxocarbon]]
-            -- vim.cmd [[colorscheme ofirkai-darkblue]]
-            require('onedark').setup {
-                style = 'deep'
-            }
-            require('onedark').load()
-
+            -- vim.cmd [[colorscheme rose-pine-main]]
+            -- vim.cmd [[colorscheme tokyodark]]
+            vim.cmd [[colorscheme ofirkai-darkblue]]
             vim.cmd [[ hi Visual guifg=#17172b ]]
             vim.cmd [[ hi Visual guibg=#e1e132 ]]
             -- vim.cmd [[ hi Normal guibg=#11131D ]]
