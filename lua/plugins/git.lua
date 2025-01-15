b71a155478794c60135e40e4e0b07b6cd19938c4
Raik Rohde <raik.rohde@eviden.com>
2025-01-15 08:40:19 +0100

    feat(treesitter): add log syntax highlighting plugin
    Integrated the log-highlight.nvim plugin to support syntax highlighting specifically for log files, enhancing readability and debugging within log files through clearer syntax distinction.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -44,11 +44,21 @@ return {
             }
         end,
     },
-    
     {
         'kdheepak/lazygit.nvim',
         keys = {
             { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
         },
     },
+    {
+        'pwntester/octo.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup()
+        end
+    }
 }
---
+++ lua/plugins/highlighting.lua
@@ -0,0 +1,52 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function(_)
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = {},
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enabled = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = false,
+                        node_decremental = "grm",
+                    },
+                },
+            }
+
+            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+            parser_config.d2 = {
+                install_info = {
+                    url = 'https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2',
+                    revision = 'main',
+                    files = { 'src/parser.c', 'src/scanner.c' },
+                },
+                filetype = 'd2',
+            };
+        end,
+        keys = {
+            {
+                '<leader>z',
+                function()
+                    vim.opt.foldmethod = 'expr'; vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+                end,
+                desc = "Load folds"
+            },
+        }
+    },
+    {
+        'fei6409/log-highlight.nvim',
+        config = function()
+            require('log-highlight').setup {}
+        end,
+    },
+}
