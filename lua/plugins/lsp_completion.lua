90ae6fed61def6fb4b6914ae0f8322dc7c9eeea8
dikkadev <raik@dikka.dev>
2025-12-05 22:29:13 +0100

    feat(lsp): enable mason-lspconfig auto server activation

--- lua/plugins/lsp_completion.lua
+++ lua/plugins/lsp_completion.lua
@@ -10,10 +10,10 @@ return {
         "williamboman/mason-lspconfig.nvim",
         dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
         config = function()
-            -- require("mason-lspconfig").setup({
-            --     ensure_installed = {},
-            --     automatic_installation = false,
-            -- })
+            require("mason-lspconfig").setup({
+                -- automatically call vim.lsp.enable() for installed servers
+                automatic_enable = true,
+            })
         end,
     },
 
